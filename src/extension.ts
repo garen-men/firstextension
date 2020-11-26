add5075da448c16216d7fa55a217b64dccfcd264
孟麟伦 <menglinl@chanjet.com>
2020-11-26 10:43:47 +0800

    add api

--- src/extension.ts
+++ src/extension.ts
@@ -1,117 +1,39 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { ExtensionContext, commands, window, workspace } from 'vscode'
+import Provider from './Provider'
+import fundHandle from './Handle'
 
-const insertText = (val:string) => {
-	const editor = vscode.window.activeTextEditor;
-
-	if (!editor) {
-		vscode.window.showErrorMessage('Can\'t insert log because no document is open');
-		return;
+// 激活插件
+export function activate(context: ExtensionContext) {
+	let interval = workspace.getConfiguration().get('fund-watch.interval', 2)
+	if (interval < 2) {
+		interval = 2
 	}
 
-	const selection = editor.selection;
+	// 基金类
+	const provider = new Provider()
 
-	const lineOfSelectedVar = selection.active.line;
+	// 数据注册
+	window.registerTreeDataProvider('fund-list', provider)
 
-	editor.edit((editBuilder) => {
-		editBuilder.insert(new vscode.Position(lineOfSelectedVar + 1, 0),val)
-	});
+	// 定时任务
+	setInterval(() => {
+		provider.refresh()
+	}, interval * 1000)
+
+	// menu 事件
+	context.subscriptions.push(
+		commands.registerCommand(`fund.add`, () => {
+			provider.addFund()
+		}),
+		commands.registerCommand(`fund.refresh`, () => {
+			provider.refresh()
+		}),
+		commands.registerCommand('fund.item.remove', (fund) => {
+			const { code } = fund
+			fundHandle.removeConfig(code)
+			provider.refresh()
+		})
+	)
 }
 
-function getAllLogStatements() {
-	const editor = vscode.window.activeTextEditor;
-	if (!editor) { return []; }
-
-	const document = editor.document;
-	const documentText = editor.document.getText();
-
-	let logStatements = [];
-
-	const logRegex = /console.(log|debug|info|warn|error|assert|dir|dirxml|trace|group|groupEnd|time|timeEnd|profile|profileEnd|count)\((.*)\);?/g;
-	let match;
-	while (match = logRegex.exec(documentText)) {
-		let matchRange =
-			new vscode.Range(
-				document.positionAt(match.index),
-				document.positionAt(match.index + match[0].length)
-			);
-		if (!matchRange.isEmpty)
-			logStatements.push(matchRange);
-	}
-	return logStatements;
-}
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {  
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "firstextension" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('firstExt.hello', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		// vscode.window.showInformationMessage('Hello World 123 !');
-		vscode.window.showInformationMessage('要不要去珠峰官网呢?', '是', '否').then(result => {
-			if (result === '是') {
-				vscode.env.openExternal(vscode.Uri.parse('http://www.zhufengpeixun.cn/'));
-			}
-		});
-		//vscode.window.show
-	});
-
-	context.subscriptions.push(disposable);
-
-	const insertLog = vscode.commands.registerCommand('firstExt.insertLog', () => {
-		const editor = vscode.window.activeTextEditor;
-		if (!editor) { return; }
-		const selection = editor.selection;
-		const text = editor.document.getText(selection);
-		const logToInsert = `console.log('${text}: ',${text});\n`;
-		text ? insertText(logToInsert) : insertText('console.log();');
-	});
-	context.subscriptions.push(insertLog);
-
-
-	const deleteAllLog = vscode.commands.registerCommand('firstExt.delLog', () => {
-		const editor = vscode.window.activeTextEditor;
-		if (!editor) { return; }
-
-		let workspaceEdit = new vscode.WorkspaceEdit();
-		const document = editor.document;
-
-		const logStatements = getAllLogStatements();
-
-		logStatements.forEach((log) => {
-			workspaceEdit.delete(document.uri, log);
-		});
-
-		vscode.workspace.applyEdit(workspaceEdit).then(() => {
-			vscode.window.showInformationMessage(`${logStatements.length} console.log deleted`);
-		});
-	});
-	context.subscriptions.push(deleteAllLog);
-
-	let removeComments = vscode.commands.registerCommand('firstExt.removeComments', function () {
-		const editor = vscode.window.activeTextEditor;
-		if (!editor) { return; }
-
-		editor.edit(editBuilder => {
-			let text = editor.document.getText();
-			text = text.replace(/((\/\*([\w\W]+?)\*\/)|(\/\/(.(?!"\)))+)|(^\s*(?=\r?$)\n))/gm, '').replace(/(^\s*(?=\r?$)\n)/gm, '').replace(/\\n\\n\?/gm, '');
-			const end = new vscode.Position(editor.document.lineCount + 1, 0);
-			editBuilder.replace(new vscode.Range(new vscode.Position(0, 0), end), text);
-			vscode.commands.executeCommand('editor.action.formatDocument');
-		});
-	});
-
-	context.subscriptions.push(removeComments);
-}
-
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
---
+++ src/utils.ts
@@ -0,0 +1,57 @@
+import * as https from 'https'
+
+// 请求
+const request = async (url: string): Promise<string> => {
+    return new Promise((resolve, reject) => {
+        https.get(url, (res) => {
+            let chunks = ''
+            if (!res || res.statusCode !== 200) {
+                reject(new Error('网络请求错误!'))
+                return
+            }
+            res.on('data', (chunk) => {
+                chunks += chunk.toString('utf8')
+            })
+            res.on('end', () => {
+                resolve(chunks)
+            })
+        })
+    })
+}
+
+export function fundApi(fundConfig: string[]): Promise<FundInfo[]> {
+    const time = Date.now()
+    const promises: Promise<string>[] = []
+
+    fundConfig.forEach((code) => {
+        const url = `https://fundgz.1234567.com.cn/js/${code}.js?rt=${time}`
+        promises.push(request(url))
+    })
+    return Promise.all(promises).then((results) => {
+        const resultArr: FundInfo[] = []
+        results.forEach((rsp: string) => {
+            const match = rsp.match(/jsonpgz\((.+)\)/)
+            if (!match || !match[1]) {
+                return
+            }
+            const str = match[1]
+            const obj = JSON.parse(str)
+            const info: FundInfo = {
+                now: obj.gsz,
+                name: obj.name,
+                code: obj.fundcode,
+                lastClose: obj.dwjz,
+                changeRate: obj.gszzl,
+                changeAmount: (obj.gsz - obj.dwjz).toFixed(4),
+            }
+            resultArr.push(info)
+        })
+        return resultArr
+    })
+}
+
+
+
+export function unique(arr: any[]) {
+    return Array.from(new Set(arr))
+}
